a6cd3215c5cc93e30d19e9ab155d0c9a889865fc
kagla <kagla@naver.com>
2025-12-26 10:22:49 +0900

    🔧 이미지 도메인에 images.unsplash.com 추가

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'phinf.pstatic.net',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
